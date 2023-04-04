51319af140d2cea735812d9126ba3bfb21a8db7a
ntllong1997 <117335949+ntllong1997@users.noreply.github.com>
2023-04-03 21:24:48 -0500

    add this.props

---
+++ src/components/App/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    )
+  }
+}
+
+export default App;
